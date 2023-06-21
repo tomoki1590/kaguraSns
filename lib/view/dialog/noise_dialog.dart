e1f5a90d9e75b05d60f20e5d781c2bfd5beff545
tomato <kaguratomato1@gmail.com>
2023-06-21 23:01:11 +0900

    修正

---
+++ lib/view/dialog/noise_dialog.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+
+class NoiseDialog extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoAlertDialog(
+      content: Column(
+        children: [],
+      ),
+    );
+  }
+}
