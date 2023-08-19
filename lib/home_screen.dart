9216a65c1f9b3abe4d9405bc43eb6f81aa1ddbf2
tomato <kaguratomato1@gmail.com>
2023-08-19 19:41:11 +0900

    homeScreen 修正

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -8,16 +8,12 @@ class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
-  _HomeScreenState createState() => _HomeScreenState();
+  State<HomeScreen> createState() => _HomeScreenState();
 }
 
 class _HomeScreenState extends State<HomeScreen> {
   int indexList = 0;
-  static final tab = [
-    const KaguraTab(),
-    const KaguraAdd(),
-    const AccountTab()
-  ];
+  static final tab = [const KaguraTab(), const KaguraAdd(), const AccountTab()];
 
   void _tappedList(int index) {
     setState(() {
@@ -34,9 +30,11 @@ class _HomeScreenState extends State<HomeScreen> {
         currentIndex: indexList,
         items: const [
           BottomNavigationBarItem(icon: Icon(Icons.home), label: 'ホーム'),
-          BottomNavigationBarItem(icon: Icon(Icons.add), label: '追加'),  
+          BottomNavigationBarItem(icon: Icon(Icons.add), label: '追加'),
           BottomNavigationBarItem(
-              icon: Icon(Icons.account_circle_rounded), label: 'アカウント'),
+            icon: Icon(Icons.account_circle_rounded),
+            label: 'アカウント',
+          ),
         ],
         selectedItemColor: Colors.amber,
         unselectedItemColor: Colors.black,
